ef9f57a2a90566b406b3a846773cb13f10d65db5
Wairioko <charlesmungai5@gmail.com>
2024-10-02 16:09:58 +0300

    Added google callback handler as well as persistgate from redux

--- src/App.js
+++ src/App.js
@@ -4,26 +4,38 @@ import HomePage from './home/page/HomePage';
 import AuthProvider  from './authProvider';
 import ChatPage from './chat/page/chatpage';
 import SingleChatPage from './chat/page/singleChat';
+import { Provider } from 'react-redux';
+import {store, persistor} from './store/index';
+import { PersistGate } from 'redux-persist/integration/react';
+import {LoginPage} from './users/pages/Login'
+import SignupPage from './users/pages/Signup'
+import SignupEmail from './users/pages/SignUpEmail';
+import GoogleCallbackHandler from './auth/auth';
+
 
 function App() {
-
-  
-
   return (
-    <Router>
-    <AuthProvider>
-      
-        <Navbar/>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path='/chat' element={<ChatPage />} />
-            <Route path='/singlechat' element={<SingleChatPage />} />
-          </Routes>
-      
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <AuthProvider>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/chat/:chatid" element={<ChatPage />} />
+              <Route path="/chat/:chatid/:name" element={<SingleChatPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/signup-email" element={<SignupEmail />} />
+              <Route path="/google/auth/callback" element={<GoogleCallbackHandler />} />
 
-      </AuthProvider>
-      </Router>
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
 export default App;
+
