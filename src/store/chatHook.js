c53697e41bf2e09c8ac0fae640942e5abe5cf808
Wairioko <charlesmungai5@gmail.com>
2024-11-03 19:49:19 +0300

    hook to listen to chat data fro server, store and update chat data in redux as it is being received

---
+++ src/store/chatHook.js
@@ -0,0 +1,30 @@
+// chatHooks.js
+export const useSSEConnection = (chatId) => {
+    const setChatSession = useChatStore(state => state.setChatSession);
+    const updateModelResponse = useChatStore(state => state.updateModelResponse);
+  
+    useEffect(() => {
+      let eventSource;
+      
+      const connectSSE = () => {
+        eventSource = CreateSSEConnection(chatId, (data) => {
+          if (data.type === 'initial') {
+            setChatSession(chatId, data);
+          } else if (data.type === 'update') {
+            data.modelResponses?.forEach(modelResponse => {
+              updateModelResponse(chatId, modelResponse.modelName, modelResponse.responses?.response);
+            });
+          }
+        });
+      };
+  
+      connectSSE();
+  
+      return () => {
+        if (eventSource) {
+          eventSource.close();
+        }
+      };
+    }, [chatId, setChatSession, updateModelResponse]);
+  };
+  
