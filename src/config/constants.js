49495bbe9e78f59e0b70606a2abf834c9e0100d9
Wairioko <charlesmungai5@gmail.com>
2024-11-03 19:14:29 +0300

    constants for fetching data through sse

---
+++ src/config/constants.js
@@ -0,0 +1,8 @@
+export const SSE_RETRY_DELAY = 3000;
+export const OFFLINE_STORAGE_KEY = 'chat_offline_data';
+export const CACHE_DURATION = 30 * 60 * 1000; // 30 minutes
+export const API_ENDPOINTS = {
+  CHAT: '/api/chats',
+  SSE: '/api/sse/chats'
+};
+
