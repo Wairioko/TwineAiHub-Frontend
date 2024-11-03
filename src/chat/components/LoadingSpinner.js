86ab5adeaab644e5464d082f4f5a68852db44f28
Wairioko <charlesmungai5@gmail.com>
2024-11-03 19:28:26 +0300

    Loading spinner for awaiting server response

---
+++ src/chat/components/LoadingSpinner.js
@@ -0,0 +1,7 @@
+ // Components
+ export const LoadingSpinner = ({ message }) => (
+    <div className="loading-spinner-container">
+        <div className="loading-spinner"></div>
+        <p>{message}</p>
+    </div>
+);
