e51001fab5cff59510a04baf58d806e5635955cf
Wairioko <charlesmungai5@gmail.com>
2024-12-01 17:59:53 +0300

    addition of terms and conditions

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import { PaddleProvider } from './paddle/PaddleProvider';
 import  {Analytics} from "@vercel/analytics/react"
 import {SpeedInsights} from "@vercel/speed-insights/react"
 import { SubscriptionManagement } from './subscribe/page/subscriptionManagement';
+import TermsAndConditions from './privacy-terms/pages/terms-conditions';
 
 
 function App() {
@@ -70,6 +71,7 @@ function App() {
               />
               <Route path='/user/subscription' element={ <SubscriptionPage />} />
               <Route path='/user/subscription/management' element={ <SubscriptionManagement />} />
+              <Route path='/terms-conditions' element={ <TermsAndConditions />} />
             </Routes>
           </AuthProvider>
         </Router>
