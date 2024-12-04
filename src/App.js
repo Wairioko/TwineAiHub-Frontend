ccc048e56b920a0cf6e753e38e9b0b505797d790
Wairioko <charlesmungai5@gmail.com>
2024-12-04 19:02:30 +0300

    removed useEffect hook that initializes Google OAuth 2.0

--- src/App.js
+++ src/App.js
@@ -41,13 +41,13 @@ function App() {
     scope: "email openid profile",
   };
 
-  useEffect(() => {
-    window.gapi.load('auth2', () => {
-      window.gapi.auth2.init({
-        client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID, // Replace with your actual client ID
-      });
-    });
-  }, []);
+  // useEffect(() => {
+  //   window.gapi.load('auth2', () => {
+  //     window.gapi.auth2.init({
+  //       client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID, // Replace with your actual client ID
+  //     });
+  //   });
+  // }, []);
   
 
   return (
