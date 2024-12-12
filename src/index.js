5d319f032f6b1fc64a22896a423917ef67ef2c4c
Wairioko <charlesmungai5@gmail.com>
2024-12-12 14:37:43 +0300

    root uncommented

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import reportWebVitals from './reportWebVitals';
 
 
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <AuthProvider {...cognitoAuthConfig}> */}
