579dcf8f7b90f9de444440ffc83afaf2bd1667b3
Wairioko <charlesmungai5@gmail.com>
2024-12-19 18:44:20 +0300

    no change

--- src/home/hooks/useGetHistory.js
+++ src/home/hooks/useGetHistory.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState} from "react";
 import { getChatHistory } from "../service/service";
 
 const useGetHistory = (isAuthenticated) => {
