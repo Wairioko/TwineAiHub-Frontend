03bf3b09f8366b83d2a741d119e584ef7eeb46b1
Wairioko <charlesmungai5@gmail.com>
2024-12-19 13:04:46 +0300

    statusCode check

--- src/users/hooks/useLogin.js
+++ src/users/hooks/useLogin.js
@@ -18,7 +18,7 @@ export const UseLogin = () => {
             const response = await userLogin(data);
             
             
-            if (response.data.status === 200) { 
+            if (response.statusCode === 200) { 
                
                 navigate('/', {replace: true}); 
             } else {
