699b28d97dadc83c8c2197e6253b39ceb0c1d959
Wairioko <charlesmungai5@gmail.com>
2025-01-10 11:17:36 +0300

    wrapping app in padddle provider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PaddleProvider } from './paddle/PaddleProvider';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
    
+   <PaddleProvider vendorId={process.env.REACT_APP_PADDLE_VENDOR_ID}>
     <App />
+   </PaddleProvider>
    
   </React.StrictMode>
 );
